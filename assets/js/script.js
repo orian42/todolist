006fc226454871075b06df1f51802fe2c9043897
Jason Ryan <164967582+orian42@users.noreply.github.com>
2024-05-12 00:12:33 -0700

    Can change status and render columns properly (MVP achieved)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+// Define global variables
+var currentID;
+var newStatus;
+
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
@@ -10,11 +14,11 @@ function generateTaskId() {
 }
 
 // Todo: create a function to create a task card
-function createTaskCard(task) {
-    const taskDate = dayjs(taskList[task].taskDueDate);
+function createTaskCard(statuslist, task) {
+    const taskDate = dayjs(statuslist[task].taskDueDate);
     const today = dayjs().format('YYYY-MM-DD');
     var cardClass
-
+    //Formatting will depend on whether the task is due today, due in the future, or overdue
     if (dayjs(taskDate).isBefore(today)) {
         cardClass = "redWarn";
     } else if (dayjs(taskDate).isAfter(today)) {
@@ -26,21 +30,39 @@ function createTaskCard(task) {
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
+    //Define variables for the three columns
     const todoCol = $('#todo-cards');
+    const progCol = $('#in-progress-cards');
+    const doneCol = $('#done-cards');
+    //filter the "tasklist" array three different ways based on the status of each task
+    let todoArray = taskList.filter(obj => obj.taskStatus === 'todo-cards');
+    let progArray = taskList.filter(obj => obj.taskStatus == 'in-progress-cards');
+    let doneArray = taskList.filter(obj => obj.taskStatus === 'done-cards');
+    //Clear the board prior to rendering
     todoCol.text('');
+    progCol.text('');
+    doneCol.text('');
+    //Inner function for rendering each column based on task status
+    function renderColumns (taskColumn, statusList) {
+        for (i=0; i<statusList.length; i++) {
+            //note the custom attribute below: "assocID" (meaning associated ID)
+            //This stores the unique task ID in the card itself to be called later
+            taskColumn.append(`<div class="task-card" assocID=${statusList[i].taskId}>`);
+            const cardData = taskColumn.children().eq(i);
 
-    for (i=0; i<taskList.length; i++) {
-        todoCol.append('<div class="task-card">');
-        const cardData = todoCol.children().eq(i);
-
-        cardData.append(`<div class='task-card-hdr'><h2>${taskList[i].taskTitle}</h2></div>`);
-        cardData.append(`<div>${taskList[i].taskDesc}</div>`);
-        cardData.append(`<div>${dayjs(taskList[i].taskDueDate).format('M/DD/YYYY')}</div>`);
-        cardData.append(`<div><button class="btn btn-danger btn-small delete-item-btn delTaskBtn" assocID=${taskList[i].taskId}>Delete</button></div>`);
-
-        cardData.addClass(createTaskCard(i));
+            cardData.append(`<div class='task-card-hdr'><h2>${statusList[i].taskTitle}</h2></div>`);
+            cardData.append(`<div>${statusList[i].taskDesc}</div>`);
+            cardData.append(`<div>${dayjs(statusList[i].taskDueDate).format('M/DD/YYYY')}</div>`);
+            cardData.append(`<div><button class="btn btn-danger btn-small delete-item-btn delTaskBtn">Delete</button></div>`);
+            //Function called to format the above elements in "post-it"-looking card in the browser
+            cardData.addClass(createTaskCard(statusList, i));
+        }
     }
-
+    //Three calls of the above inner function
+    renderColumns (todoCol, todoArray);
+    renderColumns (progCol, progArray);
+    renderColumns (doneCol, doneArray);
+    //Make the cards draggable
     $( function() {
         $('.task-card').draggable();
     } );
@@ -48,6 +70,7 @@ function renderTaskList() {
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(){
+    //Create a tasklist array if one does not already exist
     if (taskList === null) {taskList = []};
 
     const newTaskItem = {
@@ -55,9 +78,9 @@ function handleAddTask(){
         taskTitle: $('#taskTitle').val(),
         taskDueDate: $('#taskDueDate').val(),
         taskDesc: $('#taskDesc').val(),
-        taskStatus: "to-do"
+        taskStatus: "todo-cards"
     }
-
+    //append the new task to the array then save the array in local storage
     taskList.push(newTaskItem);
     localStorage.setItem('tasks', JSON.stringify(taskList));
 
@@ -66,7 +89,8 @@ function handleAddTask(){
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
-    let assocID = Number($(event.target).attr('assocID'));
+    //Custom attribute used here
+    let assocID = Number($(event.target).parent().parent().attr('assocID'));
     let filteredArray = taskList.filter(obj => obj.taskId !== assocID);
     localStorage.setItem('tasks', JSON.stringify(filteredArray));
     taskList = JSON.parse(localStorage.getItem("tasks"));
@@ -75,14 +99,21 @@ function handleDeleteTask(event){
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
-
+    const currentTask = taskList.find(obj => obj.taskId === currentID);
+    if (currentTask) {
+        currentTask.taskStatus = newStatus;
+    }
+    localStorage.setItem('tasks', JSON.stringify(taskList));
+    console.log(currentTask);
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
     renderTaskList();
 
-    $('#todo-cards').on('click', '.delTaskBtn', handleDeleteTask);
+    $('.lane').on('click', '.delTaskBtn', handleDeleteTask);
+    //Custom attribute used here
+    $('.lane').on('dragstart', '.task-card', function() {currentID = Number($(this).attr('assocID'))});
 
     $("#addTaskBtn").click(function() {
         $("#dialog-form").dialog({
@@ -105,11 +136,11 @@ $(document).ready(function () {
     $( function() {
         $('.droppable').droppable({
             drop: function( event, ui ) {
-                $( this )
-                .addClass( "ui-state-highlight" );
-            // renderTaskList();   ---------------------commented out for now
+                newStatus = $(this).children().attr('id');
+            handleDrop();
+            renderTaskList();
             }
         });
-        
     } );
+
 });
